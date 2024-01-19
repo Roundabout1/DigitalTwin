d108f70a9632ecec54638386f25d50e5f0523ddc
Roundabout1 <mrsivannap@gmail.com>
2024-01-19 16:27:06 +0800

    Удаление лишних файлов

--- src/main/java/com/mycompany/digitaltwin/FrameMainMenu.java
+++ src/main/java/com/mycompany/digitaltwin/FrameMainMenu.java
@@ -8,22 +8,16 @@ public class FrameMainMenu extends JFrame {
     
     public FrameMainMenu() {
         setTitle("My Frame");
-        setSize(400, 400);
+        setSize(200, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Create buttons
         JButton start = new JButton("Старт");
-        JButton graph1 = new JButton("График 1");
-        JButton graph2 = new JButton("График 2");
-        JButton graph3 = new JButton("График 3");
         // Add buttons to the top panel
         JPanel topPanel = new JPanel();
         topPanel.setLayout(new BoxLayout(topPanel, BoxLayout.Y_AXIS));
         topPanel.add(Box.createVerticalGlue());
         topPanel.add(start);
-        topPanel.add(graph1);
-        topPanel.add(graph2);
-        topPanel.add(graph3);
         topPanel.add(Box.createVerticalGlue());
 
 
@@ -35,22 +29,10 @@ public class FrameMainMenu extends JFrame {
             System.out.println("The start button was clicked!");
             DigitalTwin.setup();
         });
-        
-        graph1.addActionListener((ActionEvent e) -> {
-            System.out.println("The graph1 button was clicked!");
-        });
-        graph2.addActionListener((ActionEvent e) -> {
-            System.out.println("The graph2 button was clicked!");
-        });
-        graph3.addActionListener((ActionEvent e) -> {
-            System.out.println("The graph3 button was clicked!");
-        });
+      
         
         // Center the buttons horizontally
         start.setAlignmentX(Component.CENTER_ALIGNMENT);
-        graph1.setAlignmentX(Component.CENTER_ALIGNMENT);
-        graph2.setAlignmentX(Component.CENTER_ALIGNMENT);
-        graph3.setAlignmentX(Component.CENTER_ALIGNMENT);
         
         setVisible(true);
     }
